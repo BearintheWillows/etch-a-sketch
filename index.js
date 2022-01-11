07fabebebfbad0a31f53db1ca76d356c89c7ea1e
BearInTheWillows <SJWillows@outlook.com>
2022-01-11 21:06:22 +0000

    Created 16*16 grid & implemented background color change on mouseover
    grid not responsive currently

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const container = document.querySelector('.container')
-console.log(container)
-const gridSquare = document.createElement('div')
+let gridSquare
+let divs = []
+let divRow = 65
+function createRow() {
+    
+    let divColumn = 0
+    let div
+    for (let index = 0; index < 17; index++) {
+        div = document.createElement(`div`)
+        div.classList.add('grid-square', `row-${String.fromCharCode(divRow)}`, `column-${divColumn}`)
+        divs.push(div)
+        divColumn++
+    }
+}
+
+function createDiv() {
+    for (let index = 0; index < 17; index++) {
+        createRow()
+        divRow++
+        }
+}
 
 
 
 function createGrid() {
-    gridSquare.classList.add('grid-square')
-   
-    for (let index = 0; index < 16; index++) {
-    container.appendChild(gridSquare)
-    console.log('appended')
+    createDiv()
+    let id = 0
+    let docFrag = document.createDocumentFragment();
+    for (let index = 0; index < 256; index++) {
+    divs[index].setAttribute('id', `square${id}`)
+    docFrag.appendChild(divs[index])
+    id++
         }
-
-        console.log(document.querySelectorAll('.grid-square'))
+        container.appendChild(docFrag)
+        gridSquare = document.querySelectorAll('div.grid-square')
 }
 
 createGrid()
+
+const elements = document.querySelectorAll('.grid-square')
+
+elements.forEach(element => {
+    element.addEventListener('mouseover', (e) => {
+        console.log('someone hit', element)
+        element.style.background = 'blue'
+
+        setTimeout(() => {
+            element.style.background = 'white'
+        }, 400)
+            
+    })
+})
