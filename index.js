7e096ca16c2e6ca0f4643925727cc21348dcf1d6
BearintheWillows <SJWillows91@outlook.com>
2022-01-13 03:03:43 +0000

    Added custom Grid size.
    Implemented custom grid size based on prompt()

--- index.js
+++ index.js
@@ -1,61 +1,121 @@
 const container = document.querySelector('.container')
 const resetBtn = document.querySelector('button')
-let gridSquare
-let divs = []
-let divRow = 65
-function createRow() {
+let columns = []
+let rows = []
+
+function createGrid(customGrid) {
     
-    let divColumn = 0
-    let div
-    for (let index = 0; index < 17; index++) {
-        div = document.createElement(`div`)
-        div.classList.add('grid-square', `row-${String.fromCharCode(divRow)}`, `column-${divColumn}`)
-        divs.push(div)
-        divColumn++
+    let divRow = 0
+    let gridSquare
+
+
+
+
+    createDiv()
+    let id = 0
+    
+  let idNum = 1
+    let docFragRows = document.createDocumentFragment();
+    
+    for (let index = 0; index < (customGrid); index++) {
+        docFragRows.appendChild(rows[index]) 
+    
+        }
+    
+    container.appendChild(docFragRows)
+
+    for (let index = 0; index < customGrid; index++) {
+  
+        let rowDom = document.querySelector(`.row${idNum}`)
+        let docFragColumns = document.createDocumentFragment();
+        for (let index = 0; index < customGrid; index++) {
+            docFragColumns.appendChild(columns[idNum - 1][index])
+        }
+        rowDom.appendChild(docFragColumns)
+        idNum++
+    }
+
+    
+
+    gridSquare = document.querySelectorAll('div.grid-square')
+    hoverInit()
+
+
+    function hoverInit() {
+        const elements = document.querySelectorAll('.grid-square')
+        
+        
+        elements.forEach(element => {
+        element.addEventListener('mouseover', (e) => {
+            element.style.background = 'blue'
+            
+    
+            // setTimeout(() => {
+            //     element.style.background = 'white'
+            // }, 400)
+                
+        })
+    })
+        
+    }
+    
+    
+    function createRow(rowIndex) {
+        let divColumnNum = 1
+        let divColumn
+        let row = []
+        for (let index = 0; index < customGrid; index++) {
+            
+            divColumn = document.createElement('div')
+            divColumn.classList.add('grid-square', `row-${rowIndex}`, `column-${divColumnNum}`)
+            row.push(divColumn)
+            divColumnNum++
+            }
+            return row
+    }
+    
+    function createDiv() {
+        let divRow 
+    
+        for (let index = 0; index < customGrid; index++) {
+            let rowIndex = index + 1
+            
+            columns.push(createRow(rowIndex))
+    
+            divRow = document.createElement('div')
+            divRow.classList.add(`row${rowIndex}`, 'row')
+            rows.push(divRow)
+            
+            }
     }
 }
 
-function createDiv() {
-    for (let index = 0; index < 17; index++) {
-        createRow()
-        divRow++
-        }
-}
-
-
-
-function createGrid() {
-    createDiv()
-    let id = 0
-    let docFrag = document.createDocumentFragment();
-    for (let index = 0; index < 256; index++) {
-    divs[index].setAttribute('id', `square${id}`)
-    docFrag.appendChild(divs[index])
-    id++
-        }
-        container.appendChild(docFrag)
-        gridSquare = document.querySelectorAll('div.grid-square')
-}
-
-createGrid()
-
-const elements = document.querySelectorAll('.grid-square')
-
-elements.forEach(element => {
-    element.addEventListener('mouseover', (e) => {
-        element.style.background = 'blue'
-
-        // setTimeout(() => {
-        //     element.style.background = 'white'
-        // }, 400)
-            
-    })
-})
+createGrid(16)
 
 function reset() {
-        elements.forEach(element => {
-            element.style.background = 'white'
-        })
-}
+    const elements = document.querySelectorAll('.grid-square')
+    let customGrid
+    elements.forEach(element => {
+        element.style.background = 'white'
+    })
+    customGrid = prompt('Choose a grid size. 1-100')
+    
+    if (customGrid < 100 && customGrid > 1) {
+        const removeChilds = (parent) => {
+        while (parent.lastChild) {
+            parent.removeChild(parent.lastChild)
+        }
+    }
 
+    rows = []
+    columns = []
+    removeChilds(container)
+    createGrid(customGrid)
+    } else {
+        alert('Selection out of bounds. Choose a number between 1-100')
+        reset()
+    }
+
+    
+}
 resetBtn.addEventListener('click', reset)
